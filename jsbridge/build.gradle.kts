18ed3931f31f9e01f75d9c54ffaa265a60e79a03
Sunhy <sunhy.top@foxmail.com>
2024-05-06 11:14:17 +0800

    build: 引入发布工具以及相关实现；

--- jsbridge/build.gradle.kts
+++ jsbridge/build.gradle.kts
@@ -1,7 +1,12 @@
+import com.android.build.api.variant.LibraryAndroidComponentsExtension
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
+    id("signing")
+    id("com.vanniktech.maven.publish")
 }
 
 android {
@@ -9,6 +14,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
+        minSdk = 19
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -23,25 +29,84 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
+
 }
 
 dependencies {
     compileOnly("com.google.code.gson:gson:2.10.1")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "top.sunhy.component"
-            artifactId = "jsbridge"
-            version = "1.0.0"
+val pomGroupId = properties["GROUP"] as String
+val pomArtifactId = properties["POM_ARTIFACT_ID"] as String
+val pomVersion = properties["VERSION_NAME"] as String
+val pomDesc = properties["POM_DESCRIPTION"] as String
+val pomUrl = properties["POM_URL"] as String
+val packaging = properties["POM_PACKAGING"] as String
+val developerId = properties["POM_DEVELOPER_ID"] as String
+val developerName = properties["POM_DEVELOPER_NAME"] as String
+val developerEmail = properties["POM_DEVELOPER_EMAIL"] as String
 
-            afterEvaluate {
-                from(components["release"])
+afterEvaluate {
+    if (packaging == "aar") {
+        extensions.getByType(LibraryAndroidComponentsExtension::class).finalizeDsl {
+            it.publishing {
+                singleVariant("release") {
+                    withSourcesJar()
+                }
+            }
+        }
+    } else {
+        extensions.getByType<JavaPluginExtension>().apply {
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+    extensions.configure<PublishingExtension>("publishing") {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                this.groupId = pomGroupId
+                this.artifactId = pomArtifactId
+                this.version = pomVersion
+                when (packaging) {
+                    "aar" -> from(components["release"])
+                    "jar" -> from(components["java"])
+                }
+                pom {
+                    this.name.set(pomArtifactId)
+                    this.description.set(pomDesc)
+                    this.packaging = packaging
+                    this.url.set(pomUrl)
+                    developers {
+                        developer {
+                            this.id.set(developerId)
+                            this.name.set(developerName)
+                            this.email.set(developerEmail)
+                        }
+                    }
+                }
+            }
+        }
+        repositories {
+            maven {
+                name = "local"
+                url = File(rootDir, "repos").toURI()
             }
         }
     }
 }
+
+mavenPublishing {
+    configure(
+        AndroidSingleVariantLibrary(
+            variant = "release",
+            sourcesJar = true,
+            publishJavadocJar = true,
+        )
+    )
+}
+
+
