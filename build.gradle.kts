835d3a6c03157aeb663ba5e0b19b002cee8eda9c
Sunhy <sunhy.top@foxmail.com>
2025-04-14 12:00:18 +0800

    build: 项目 apg 插件更新至 8.7.3；

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.library") version "8.7.3" apply false
     id("com.vanniktech.maven.publish") version "0.28.0"
 }
